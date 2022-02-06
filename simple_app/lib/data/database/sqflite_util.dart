45a5cfcbe5c01c18094637629c60f2ce587b29cc
LeoAndo <leo.777.info@gmail.com>
2022-02-06 21:19:15 +0900

    sqlite

---
+++ simple_app/lib/data/database/sqflite_util.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+/// create the folder and returnes its path
+Future<String> initDb(String dbName) async {
+  final databasePath = await getDatabasesPath();
+  final path = join(databasePath, dbName);
+  if (await Directory(dirname(path)).exists() == false) {
+    try {
+      await Directory(dirname(path)).create(recursive: true);
+    } catch (e) {
+      print(e);
+    }
+  }
+  return path;
+}
