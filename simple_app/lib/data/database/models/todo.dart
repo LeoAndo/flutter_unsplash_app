c3646ae0f9ac1c000d07cf3676f904254977b489
LeoAndo <leo.777.info@gmail.com>
2022-02-06 17:11:38 +0900

    2. Define the data model

---
+++ simple_app/lib/data/database/models/todo.dart
@@ -0,0 +1,12 @@
+class Todo {
+  final int id;
+  final String title;
+  final String subtitle;
+  final bool isDone;
+
+  Todo(
+      {required this.id,
+      required this.title,
+      required this.subtitle,
+      required this.isDone});
+}
